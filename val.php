067f03b92c34da69a95cedf1db3827c286a3b01f
Vrish09 <vrishr@gmail.com>
2014-12-30 16:39:31 +1100

    New Work
    New work onmath and val files

--- val.php
+++ val.php
@@ -29,7 +29,7 @@ if($an)
 }else{
     echo $total=$num1+$an;
 }
-*/
+
        //session_start();
        $num1 = $_POST['num1'];
        $_SESSION['num1']=$_POST['num1'];
@@ -41,6 +41,98 @@ echo $total=$num1 + $num2;
 }else{
  echo" false";   
 }
+*/
+$num1 = $_POST['num1'];
+//echo $num1;
+$length=strlen($num1);
+//echo "<br>".$length."<br>";
+$total=0;
+if(strpos($num1, '+'))
+{
+    //echo "In strpos";
+    for($i=0;$i<$length;$i++)
+    {
+        //echo $num1{$i};
+        if(is_numeric($num1{$i}))
+        {
+                  
+            $total=$total + $num1{$i};
+           
+        }
+        if(strpos($num1{$i},'+'))
+           {
+           // echo "Good";
+            //echo "<br>". $i;
+           }
+    }
+            echo "<br>". $total;
+}
+$strval="";
+if(preg_match('/^Sin*/', $num1))
+{        
+    for($j=0;$j<strlen($num1);$j++)
+    {
+        if(is_numeric($num1{$j}))
+            {                 
+                $strval=$strval.$num1{$j};
+                //$total1= sin{$num1};           
+            }
+        }
+    echo sin($strval);
+    echo "<br>".sinh($strval);
+}
+$strval="";
+if(preg_match('/^Cos*/', $num1))
+{        
+    for($j=0;$j<strlen($num1);$j++)
+    {
+        if(is_numeric($num1{$j}))
+            {                 
+                $strval=$strval.$num1{$j};
+                //$total1= sin{$num1};           
+            }
+        }
+    echo cos($strval);
+    echo "<br>".cosh($strval);
+}
+$strval="";
+if(preg_match('/^Tan*/', $num1))
+{        
+    for($j=0;$j<strlen($num1);$j++)
+    {
+        if(is_numeric($num1{$j}))
+            {                 
+                $strval=$strval.$num1{$j};
+                //$total1= sin{$num1};           
+            }
+        }
+    echo tan($strval);
+    echo "<br>".tanh($strval);
+}
 
 
+/*      
+$num_array=array();
+function stringtoarray($s)
+{
+    $r=array();
+    for($i=0;$i<strlen($s);$i++)
+    {
+        if(is_numeric($s[$i]) )
+        {
+            $r[$i]=$s[$i];
+            if ()
+        }
+    }
+    return $r; 
+}
+stringtoarray($num1);
+$num_array=stringtoarray($num1);
+for($i=0;$i<count($num_array);$i++){
+    //echo $num_array[$i];
+    var_dump($num_array);
+    print_r($num_array);
+}
+*/
 ?>
+
