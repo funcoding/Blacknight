0682c178723aa576afe507f3bfb5c6c14a96f3b8
Vrish09 <vrishr@gmail.com>
2015-01-05 20:21:19 +1100

    New Work
    Using function to do task 1 and task 2.  The other bit of my code not
    working to well. If u find some loop holes do tell me.

--- val.php
+++ val.php
@@ -1,117 +1,126 @@
-<?php
-	//addition(nu1+nu2);
+<html>
+    <head>
+        <title>Title goes here</title>
+    </head>
+    <body>
+        <form action="val.php" method="post">
+            <label>Enter Num1:</label>
+            <input type="text" name="num1"><br>
+            <input  type="submit" name="add_submit" value="Add">
+        </form>
+
+        <?php       
+            $num1 = $_POST['num1'];
+            /*
+            //For Addition
+            //echo $num1;
+            $length=strlen($num1);
+            //echo "<br>".$length."<br>";
+            $total=0;
+
+            if(strpos($num1, '+'))
+            {
+                for($i=0;$i<$length;$i++)
+                {
+                    if(is_numeric($num1{$i}))
+                    { 
+                    $number=trim($number);
+                    $number=preg_replace('[^0-9\+-\*\/\(\) ]', '', $number);
+                       $total = $total + $number;         
+                    }
+                }
+                 echo "<br>". "Result:". $total;
+            }
+            */
+function calculate_string($mathsString)
+{
+    $mathsString=trim($mathsString);
+    $mathsString=preg_replace('[^0-9\+-\*\/\(\)\sin\cos\tan\ ]', '', $mathsString);
+    $compute=create_function("","return (" . $mathsString . ");" );
+    return 0 + $compute();
+}
+    $string=$num1;
+    echo "<br>".calculate_string($string);
 /*
-	if (addition) {
-     $addition = true;
-	  echo "I can add up!";
-     $add = $nu1 + $nu2;
-    } else {
-     $addition = false;
-	  echo"bad numbers to add up";
-    }
-    */
-        /*
+ //For Subtraction 
         $num1 = $_POST['num1'];
-        $num2 = $_POST['num2'];
-    if($_POST['add_submit']){
-$total=true;
-echo"Can add up";
-echo $total=$num1+$num2;
-}else{
-	$total=false;
-	echo"Not possible error";
-}
-$an=array($num2);
-if($an)
-{
-    empty($an);
-    echo "Enter";
-}else{
-    echo $total=$num1+$an;
-}
-
-       //session_start();
-       $num1 = $_POST['num1'];
-       $_SESSION['num1']=$_POST['num1'];
-        $num2 = $_POST['num2'];
-        $_SESSION['num2'] = $_POST['num2'];
-    if($_POST['add_submit']){
-$total=true;
-echo $total=$num1 + $num2;
-}else{
- echo" false";   
-}
-*/
-$num1 = $_POST['num1'];
-//echo $num1;
-$length=strlen($num1);
-//echo "<br>".$length."<br>";
-$total=0;
-if(strpos($num1, '+'))
-{
-    //echo "In strpos";
-    for($i=0;$i<$length;$i++)
-    {
-        //echo $num1{$i};
-        if(is_numeric($num1{$i}))
+        //echo $num1;
+        $length=strlen($num1);
+        //echo "<br>".$length."<br>";
+        $total=0;
+        if(strpos($num1, '-'))
         {
-                  
-            $total=$total + $num1{$i};
-           
-        }
-        if(strpos($num1{$i},'+'))
-           {
-           // echo "Good";
-            //echo "<br>". $i;
-           }
-    }
-            echo "<br>". $total;
-}
-$strval="";
-if(preg_match('/^Sin*/', $num1))
-{        
-    for($j=0;$j<strlen($num1);$j++)
-    {
-        if(is_numeric($num1{$j}))
-            {                 
-                $strval=$strval.$num1{$j};
-                //$total1= sin{$num1};           
+            for($i=0;$i<$length;$i++)
+            {
+                //echo $num1{$i};
+                if(is_numeric($num1{$i}))
+                {              
+                    $total=$num1{$i} - $num1;           
+                }
+                if(strpos($num1{$i},'-')){}
             }
+             echo "<br>". "Result:". $total;
+*/
+        //}
+        $strval="";
+        if(preg_match('/^Sin*/', $num1))
+        {        
+            for($j=0;$j<strlen($num1);$j++)
+            {
+                if(is_numeric($num1{$j}))
+                    {                 
+                        $strval=$strval.$num1{$j};          
+                    }
+                }
+            echo sin($strval);
+            echo "<br>".sinh($strval);
         }
-    echo sin($strval);
-    echo "<br>".sinh($strval);
-}
-$strval="";
-if(preg_match('/^Cos*/', $num1))
-{        
-    for($j=0;$j<strlen($num1);$j++)
-    {
-        if(is_numeric($num1{$j}))
-            {                 
-                $strval=$strval.$num1{$j};
-                //$total1= sin{$num1};           
-            }
+        $strval="";
+        if(preg_match('/^Cos*/', $num1))
+        {        
+            for($j=0;$j<strlen($num1);$j++)
+            {
+                if(is_numeric($num1{$j}))
+                    {                 
+                        $strval=$strval.$num1{$j};          
+                    }
+                }
+            echo cos($strval);
+            echo "<br>".cosh($strval);
         }
-    echo cos($strval);
-    echo "<br>".cosh($strval);
-}
-$strval="";
-if(preg_match('/^Tan*/', $num1))
-{        
-    for($j=0;$j<strlen($num1);$j++)
-    {
-        if(is_numeric($num1{$j}))
-            {                 
-                $strval=$strval.$num1{$j};
-                //$total1= sin{$num1};           
-            }
+        $strval="";
+        if(preg_match('/^Tan*/', $num1))
+        {        
+            for($j=0;$j<strlen($num1);$j++)
+            {
+                if(is_numeric($num1{$j}))
+                    {                 
+                        $strval=$strval.$num1{$j};          
+                    }
+                }
+            echo tan($strval);
+            echo "<br>".tanh($strval);
         }
-    echo tan($strval);
-    echo "<br>".tanh($strval);
-}
-
-
-/*      
+        $strval="";
+        if(preg_match('/^Log*/', $num1))
+        {        
+            for($j=0;$j<strlen($num1);$j++)
+            {
+                if(is_numeric($num1{$j}))
+                    {                 
+                        $strval=$strval.$num1{$j};           
+                    }
+                }
+            echo log($strval);
+            echo "<br>".log10($strval);
+            echo "<br>".log1p($strval);
+        }
+        ?>
+    </body>
+</html>
+<?php
+/*  
+//For addition using arrays    
 $num_array=array();
 function stringtoarray($s)
 {
@@ -135,4 +144,3 @@ for($i=0;$i<count($num_array);$i++){
 }
 */
 ?>
-
